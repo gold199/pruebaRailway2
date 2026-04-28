c31967c86abc24cb6e5df9fee6f38cebede967e6
gold199 <151873371+gold199@users.noreply.github.com>
2026-04-28 09:12:22 +0200

    Add files via upload

--- api/app.mjs
+++ api/app.mjs
@@ -25,7 +25,7 @@ app.use(apiSecurity.apiLimiter);
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://pruebarailway-production-229f.up.railway.app/",
+    origin: "https://pruebarailway-production-229f.up.railway.app:3001",
     credentials: true,
   }),
 );
