02a8bf347aafcfb00505f954e04c929ccb35a85f
gold199 <151873371+gold199@users.noreply.github.com>
2026-04-28 10:11:44 +0200

    fsedsdsdf

--- api/app.mjs
+++ api/app.mjs
@@ -25,7 +25,7 @@ app.use(userAgent.express());
 // app.use(cors());
 app.use(
   cors({
-    origin: "pruebarailway-production-a4d1.up.railway.app",
+    origin: "https://pruebarailway-production-a4d1.up.railway.app",
     credentials: true,
   }),
 );
