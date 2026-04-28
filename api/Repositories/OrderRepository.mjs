90d1112b60a36d473b3358de67c5bd96c3300b94
gold199 <151873371+gold199@users.noreply.github.com>
2026-04-28 09:12:43 +0200

    Add files via upload

--- api/Repositories/OrderRepository.mjs
+++ api/Repositories/OrderRepository.mjs
@@ -160,7 +160,7 @@ async function payment(items,user){
   const session = await stripe.checkout.sessions.create({
     line_items: arrayStripeObjects,
     mode: 'payment',
-    success_url: 'https://pruebarailway-production-229f.up.railway.app//user/myOrders',
+    success_url: 'https://pruebarailway-production-229f.up.railway.app:3001//user/myOrders',
   })
   
   sendGmailTest(validatedItems,user,total)
